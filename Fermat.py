dc91e0f9043b014609ba26f746fc1df20fd2c446
James Doe <mcf1pokerspeed@gmail.com>
2013-01-29 19:27:31 +0000

    Merged the Carmichael/Fermat tests together.
    Added to a class, this will be useful in future when the menu is
    created.

--- Fermat.py
+++ Fermat.py
@@ -1,39 +1,55 @@
-# Name: James Dietz
-# Date: 11/12/2012
-# Comment: Fermat's PRP test.
-import random
+# Carmichael number algorithm using trial division
+# Found using Korselt's criterion
 
-def test():
-    print("Program to perform the Fermat PRP test in Python.")
-    n = int(input("Please enter the number to test: "))
-    print()
-    prp = []
-    composite = 0
-    print("The chance of failure for the Fermat PRP test is ")
-    print("1/2^-k where k is the number of iterations of the test performed. ")
-    testStrength = int(input("How many iterations would you like to run?: "))
-    iterations = 0
-    if n % 2 == 0:
-        print("{0} is composite - it is divisible by two.".format(n))
-        exit
-    else:
-        pass
-    while iterations <= testStrength:
-        iterations += 1
-        base = random.randrange(1,4*testStrength)
-        number_to_test = base**(n-1)
-        if number_to_test % n == 1:
-            print("{0} is a base {1}-PRP.".format(n, base))
-        else:
-            composite = 1
-            if len(prp) > 0:
-                print(prp)
-                print("These bases threw up a false positive. \n")
+class Fermat:
+    import random
+
+    def test(number):
+        print("Fermat's PRP test.")
+        n = int(input("Please enter a number: "))
+        for c in range(1, round(n**0.5)1):
+            if n % c == 0:
+                # Check if c-1 divides n - 1
+                p = c - 1
+                m = n - 1
+                if p % m == 0:
+                    print(n, "is a Carmichael number.")
+                    quit()
+                else:
+                    pass
             else:
-                print("{0} is composite.".format(n))
-            break
-    if iterations == testStrength and composite == 0:
-        print("{0} is a PRP for bases {1}".format(n, bases))
-    else:
-        pass
-test()
+                continue
+
+        print()
+        prp = []
+        composite = 0
+        print("The chance of failure for the Fermat PRP test is\n 2^-k, where k is the number of iterations performed. ")
+        testStrength = int(input("How many iterations would you like to run?: "))
+        iterations = 0
+        if n % 2 == 0:
+            print("{0} is composite - it is divisible by two.".format(n))
+            quit()
+        else:
+            pass
+
+        # Main testing loop
+        while iterations <= testStrength:
+            iterations += 1
+            base = random.randrange(1,4*testStrength)
+            number_to_test = base**(n-1)
+            if number_to_test % n == 1:
+                print("{0} is a base {1}-PRP.".format(n, base))
+            else:
+                composite = 1
+                if len(prp) > 0:
+                    print(prp)
+                    print("These bases threw up a false positive. \n")
+                else:
+                    print("{0} is composite.".format(n))
+                break
+            
+        if iterations == testStrength and composite == 0:
+            print("{0} is a PRP for bases {1}".format(n, bases))
+        else:
+            pass
+    
